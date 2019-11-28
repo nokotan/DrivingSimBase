f319a2ae757bcc4325cf411e010aa83f5213ae59
nokotan <nokotan@dummy.com>
2019-11-28 16:17:00 +0900

    Initial Commit

---
+++ Assets/CarLights/WinkerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class WinkerControl : MonoBehaviour
+{
+    public CarController car; // reference to the car controller, must be dragged in inspector
+
+    [SerializeField] bool IsLeft;
+    private Renderer m_Renderer;
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        // enable the Renderer when the car is braking, disable it otherwise.
+        m_Renderer.enabled = (IsLeft ? car.LeftWinkerOn : car.RightWinkerOn) && Time.time % 0.8f < 0.4f;
+    }
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarList.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityStandardAssets.Utility;
+
+public class CarList : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] int ConsistencyCheckIntervalFrames = 15;
+
+    [Header("Debug")]
+    [SerializeField] List<WaypointProgressTracker> m_AllCars;
+
+    public List<WaypointProgressTracker> AllCars => m_AllCars;
+
+    // Singleton Support
+    public static CarList Instance { get; private set; }
+
+    void Start()
+    {
+        Instance = this;
+
+        m_AllCars.AddRange(FindObjectsOfType<WaypointProgressTracker>());
+        BuildAllCarsConsistency();
+    }
+
+    // 並べなおします
+    void BuildAllCarsConsistency()
+    {
+        m_AllCars.Sort((a, b) => (int)Mathf.Sign(a.progressDistance - b.progressDistance));
+    }
+
+    void Update()
+    {
+        if (Time.frameCount % ConsistencyCheckIntervalFrames == 0)
+        {
+            BuildAllCarsConsistency();
+        }
+    }
+
+    public enum FindCarOption
+    {
+        InSameLane,
+        InDifferentLane
+    }
+    
+    public WaypointProgressTracker FindAheadCar(WaypointProgressTracker thisCar, FindCarOption option = FindCarOption.InSameLane)
+    {
+        var listIndex = m_AllCars.IndexOf(thisCar);
+
+        for (int i = listIndex + 1; i < m_AllCars.Count; i++)
+        {
+            if (m_AllCars[i].Circuit == thisCar.Circuit ^ option != FindCarOption.InSameLane)
+            {
+                return m_AllCars[i];
+            }
+        }
+
+        return null;
+    }
+
+    public WaypointProgressTracker FindBehindCar(WaypointProgressTracker thisCar, FindCarOption option = FindCarOption.InSameLane)
+    {
+        var listIndex = m_AllCars.IndexOf(thisCar);
+
+        for (int i = listIndex - 1; i >= 0; i--)
+        {
+            if (m_AllCars[i].Circuit == thisCar.Circuit ^ option != FindCarOption.InSameLane)
+            {
+                return m_AllCars[i];
+            }
+        }
+
+        return null;
+    }
+
+    public float GetBehindGap(WaypointProgressTracker thisCar)
+    {
+        var behindCar = FindBehindCar(thisCar);
+        return behindCar != null ? Vector3.Magnitude(thisCar.transform.position - behindCar.transform.position) : float.PositiveInfinity;
+    }
+
+    public float GetAheadGap(WaypointProgressTracker thisCar)
+    {
+        var aheadCar = FindAheadCar(thisCar);
+        return aheadCar != null ? Vector3.Magnitude(thisCar.transform.position - aheadCar.transform.position) : float.PositiveInfinity;
+    }
+}
