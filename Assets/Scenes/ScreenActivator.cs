d2f4c267c01f2d1df54c00e4184c2a16adf56e9e
nokotan <nokotan@dummy.com>
2019-12-19 15:44:14 +0900

    マルチディスプレイ対応

---
+++ Assets/Scenes/ScreenActivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScreenActivator
+{
+    [RuntimeInitializeOnLoadMethod()]
+    public static void OnStart()
+    {
+        int count = Mathf.Min(Display.displays.Length, 3);
+
+        for (int i = 0; i < count; ++i)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
