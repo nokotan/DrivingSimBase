08d15c508a3b9df481aa145bca8dc2c9a15af8f8
nokotan <dummy@dummy.com>
2020-01-23 17:55:14 +0900

    クリープ現象の追加

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,7 +8,8 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-
+        [SerializeField] private float m_CreapFactor = 0.01f;
+        [SerializeField] private float m_CreapSpeedMax = 8.0f;
 
         private void Awake()
         {
@@ -23,6 +24,8 @@ namespace UnityStandardAssets.Vehicles.Car
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
 
+            float creapAmount = Mathf.Max(m_CreapSpeedMax - m_Car.CurrentSpeed, 0.0f) * 0.2f * m_CreapFactor;
+
             if (!m_Car.LeftWinkerOn)
                 m_Car.LeftWinkerOn |= CrossPlatformInputManager.GetButtonDown("Fire1");
             else
@@ -35,9 +38,9 @@ namespace UnityStandardAssets.Vehicles.Car
 
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
+            m_Car.Move(h, v + creapAmount, v, handbrake);
 #else
-            m_Car.Move(h, v, v, 0f);
+            m_Car.Move(h, v + creapAmount, v, 0f);
 #endif
         }
     }
